c0fa6e428da3e4a7b116b68fc7d45850c8cf7c13
unknown <yanis991@hotmail.com>
2023-03-14 15:14:31 +0000

    Create for in loop to iterate through each ingredient type

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const pastaFactory = {
     cheeseType: [Parmesan, Ricotta, Feta, Mozzarella, Cheddar]
 }
 
+
+for (let ingredient in pastaFactory) {
+
+    switch (ingredient) {
+        case 'pastaType':
+            pastaRecipe.push(`You Will Make: ${} pasta`)
+        case 'sauceType':
+            pastaRecipe.push(`with a: ${} sauce`)
+        case 'cheeseType':
+            pastaRecipe.push(`Topped with some: ${} cheese`)
+        // default:
+
+    }
+}
