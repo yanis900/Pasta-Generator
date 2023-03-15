a26b446348b0645dab43cfbf956a55a41a895f70
yanis900 <yanis991@hotmail.com>
2023-03-15 00:23:37 +0000

    complete code

--- script.js
+++ script.js
@@ -3,23 +3,29 @@ const RandomNumGenerator = num => {
 }
 
 const pastaFactory = {
-    pastaType: [Lasagna, Rigatoni, Penne, Farfalle, Spaghetti],
-    sauceType: [Marinara, Alfredo, Bolognese, Pesto, Bechamel],
-    cheeseType: [Parmesan, Ricotta, Feta, Mozzarella, Cheddar]
+    pastaType: ['Lasagna', 'Rigatoni', 'Penne', 'Farfalle', 'Spaghetti'],
+    sauceType: ['Marinara', 'Alfredo', 'Bolognese', 'Pesto', 'Bechamel'],
+    cheeseType: ['Parmesan', 'Ricotta', 'Feta', 'Mozzarella', 'Cheddar']
 }
 
 let pastaRecipe = [];
 
 for (let ingredient in pastaFactory) {
+    const ingredientIndex = RandomNumGenerator(pastaFactory[ingredient].length)
 
     switch (ingredient) {
         case 'pastaType':
-            pastaRecipe.push(`You Will Make: ${} pasta`)
+            pastaRecipe.push(`You Will Make: ${pastaFactory[ingredient][ingredientIndex]} pasta`)
+            break
         case 'sauceType':
-            pastaRecipe.push(`with a: ${} sauce`)
+            pastaRecipe.push(`with a: ${pastaFactory[ingredient][ingredientIndex]} sauce`)
+            break
         case 'cheeseType':
-            pastaRecipe.push(`Topped with some: ${} cheese`)
+            pastaRecipe.push(`Topped with some: ${pastaFactory[ingredient][ingredientIndex]} cheese`)
+            break
         // default:
 
     }
 }
+
+console.log(pastaRecipe)
